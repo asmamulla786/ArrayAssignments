ceb7b29875c4603867566ca449c84198d013f554
AsmaMulla <183721570+asmamulla786@users.noreply.github.com>
2024-12-31 13:57:44 +0530

    Added isStrictlyDescending function  and tests for that

--- assignment2/02_strictly_descending.js
+++ assignment2/02_strictly_descending.js
@@ -13,36 +13,3 @@ export function isStrictlyDescending(numbers) {
 
   return true;
 }
-
-// --------------------Testing section starts from here ----------------------
-
-function addQuote(textToQuote) {
-  return "[" + textToQuote + "]";
-}
-
-function makeMessage(numbers, actual, expected) {
-  let message = "the array of numbers  " + addQuote(numbers) + " is in";
-  message += " strictly descending order  expected  :: " + addQuote(expected);
-  message = message + " actual :: " + addQuote(actual);
-
-  return getCheckMark(actual, expected) + message;
-}
-
-function getCheckMark(actual, expected) {
-  return actual === expected ? "✅" : "❌";
-}
-
-function testStrictlyDescending(numbers, expected) {
-  const actual = isStrictlyDescending(numbers);
-  const message = makeMessage(numbers, actual, expected);
-  console.log(message);
-}
-
-function testingFunction() {
-  testStrictlyDescending([5, 4, 3, 2, 1], true);
-  testStrictlyDescending([], true);
-  testStrictlyDescending([1, 3, 4, 5, 2], false);
-  testStrictlyDescending([4, 3, 3, 1], false);
-}
-
-// testingFunction();
--- assignment2/02_strictly_descending_test.js
+++ assignment2/02_strictly_descending_test.js
@@ -1,21 +1,18 @@
-import { assertEquals } from "jsr:@std/assert";
+import { assertFalse, assert } from "jsr:@std/assert";
 import { isStrictlyDescending } from "./02_strictly_descending.js";
 
-Deno.test("strictly descending array is in descending order", () => {
-  assertEquals(isStrictlyDescending([5, 4, 3, 2, 1]), true);
+Deno.test("should be true when array in descending order", () => {
+  assert(isStrictlyDescending([5, 4, 3, 2, 1]));
 });
 
-Deno.test("strictly descending with empty array", () => {
-  assertEquals(isStrictlyDescending([]), true);
+Deno.test("should be true when array is empty", () => {
+  assert(isStrictlyDescending([]));
 });
 
-Deno.test("strictly descending array not in descending order", () => {
-  assertEquals(isStrictlyDescending([1, 3, 4, 5, 2]), false);
+Deno.test("should be false when array in not descending order", () => {
+  assertFalse(isStrictlyDescending([1, 3, 4, 5, 2]));
 });
 
-Deno.test("strictly descending array which contains same elements", () => {
-  assertEquals(isStrictlyDescending([4, 3, 3, 1]), false);
+Deno.test("should be false when array contains same elements", () => {
+  assertFalse(isStrictlyDescending([5, 4, 3, 3, 1]));
 });
-
-// testStrictlyDescending([1, 3, 4, 5, 2], false);
-// testStrictlyDescending([4, 3, 3, 1], false);
