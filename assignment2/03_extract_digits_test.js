979dced1f90b4ddf00769e482e101456ed861f66
AsmaMulla <183721570+asmamulla786@users.noreply.github.com>
2024-12-31 14:27:14 +0530

    Added extractDigits function and tests for that

--- assignment2/03_extract_digits_test.js
+++ assignment2/03_extract_digits_test.js
@@ -1,6 +1,13 @@
 import { assertEquals } from "jsr:@std/assert";
 import { extractDigits } from "./03_extract_digits.js";
 
+Deno.test(
+  "extract digits of single digit number should be that digit itself",
+  () => {
+    assertEquals(extractDigits(3), [3]);
+  }
+);
+
 Deno.test("extract digits of positive number", () => {
   assertEquals(extractDigits(123), [1, 2, 3]);
 });
